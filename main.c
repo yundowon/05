b81a8e47095295631e50e2fa66223d92cfb5d48b
yundowon <70631088+yundowon@users.noreply.github.com>
2020-10-06 16:06:54 +0900

    02

--- main.c
+++ main.c
@@ -7,15 +7,13 @@ int main(int argc, char *argv[]) {
 	
 	int i;
 	
-	printf("정수 하나를 입렵하시요. :");
+	printf("정수 하나를 입력하시요.:");
 	scanf("%i", &i);
 	
-	if(i > 0)
-		printf("양수입니다.\n");
-	else if (i < 0)
-		printf("음수입니다.\n");
-	else
-		printf("0입니다.\n");
+	if(i < 0)
+		i = i*(-1);
+	
+	printf("절대값은 %i 입니다.\n", i);
 	
 	return 0;
 }
